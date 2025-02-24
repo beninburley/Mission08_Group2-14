b02025b8bea69c365036c9c3965889e1f6978068
beninburley <130599399+beninburley@users.noreply.github.com>
2025-02-24 15:39:06 -0700

    Add project files.

---
+++ Mission08_Group2-14/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Mission08_Group2_14.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
