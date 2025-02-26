3056993371dd2d9a85c410995b95309fac4cb9e7
daynes13 <daynes13@SOPCS-1032206>
2025-02-26 09:08:27 -0700

    Jefferson's First Try

---
+++ Mission08_Group2-14/Models/QuadrantContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Mission08_Group2_14.Models
+{
+    public class QuadrantContext : DbContext
+    {
+        public QuadrantContext(DbContextOptions<QuadrantContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Tasks> Tasks { get; set; }
+        public DbSet<Categories> Categories { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Categories>().HasData(
+                new Categories { CategoryId = 1, CategoryName = "Home" },
+                new Categories { CategoryId = 2, CategoryName = "School" },
+                new Categories { CategoryId = 3, CategoryName = "Work" },
+                new Categories { CategoryId = 4, CategoryName = "Church" }
+                );
+        }
+    }
+}
